530047e614983ffab49d5b8b9e50ef40d851ed36
nekit1338 <47562425+nekit1338@users.noreply.github.com>
2024-10-23 17:36:50 +0500

    Add files via upload

---
+++ module_12_01_changed.py
@@ -0,0 +1,33 @@
+import unittest
+from Runner import Runner
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, "Повезло")
+    def test_walk(self):
+        runner = Runner("Egor")
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50, "Правильно")
+
+    @unittest.skipIf(is_frozen, "Повезло")
+    def test_run(self):
+        runner = Runner("Igor")
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100, "Правильно")
+
+    @unittest.skipIf(is_frozen, "Повезло")
+    def test_challenge(self):
+        runner1 = Runner("Anton")
+        runner2 = Runner("Evgen")
+        for _ in range(10):
+            runner1.run()
+            runner2.walk()
+        self.assertNotEqual(runner1.distance, runner2.distance, "Неправильно")
+
+
+if __name__ == '__main__':
+    unittest.main()
--- module_12_04.py
+++ module_12_04.py
@@ -1,6 +1,6 @@
 import unittest
-from module_12_01 import RunnerTest
-from module_12_03 import Tournament_Test
+from module_12_01_changed import RunnerTest
+from module_12_03_changed import Tournament_Test
 
 testST = unittest.TestSuite()
 testST.addTest(unittest.TestLoader().loadTestsFromTestCase(RunnerTest))
